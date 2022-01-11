ae5b01b7c5b186141e0ed30a8dd0909addf39e0f
wnebyte <emilssonpost@gmail.com>
2022-01-11 04:40:15 +0100

    updated import statements

--- src/main/java/com/github/wnebyte/engine/core/scene/LevelEditorScene.java
+++ src/main/java/com/github/wnebyte/engine/core/scene/LevelEditorScene.java
@@ -1,13 +1,11 @@
 package com.github.wnebyte.engine.core.scene;
 
+import org.joml.Vector2f;
 import com.github.wnebyte.engine.core.camera.Camera;
-import com.github.wnebyte.engine.components.SpriteRenderer;
 import com.github.wnebyte.engine.core.ecs.GameObject;
 import com.github.wnebyte.engine.core.ecs.Transform;
-import com.github.wnebyte.engine.renderer.Texture;
+import com.github.wnebyte.engine.components.SpriteRenderer;
 import com.github.wnebyte.engine.util.ResourceFlyWeight;
-import org.joml.Vector2f;
-import org.joml.Vector4f;
 
 public class LevelEditorScene extends Scene {
 
@@ -40,27 +38,5 @@ public class LevelEditorScene extends Scene {
 
         this.renderer.render();
     }
-
-    /*
-        int xOffset = 10;
-        int yOffset = 10;
-
-        float totalWidth = (float)(600 - xOffset * 2);
-        float totalHeight = (float)(300 - yOffset * 2);
-        float sizeX = totalWidth / 100.0f;
-        float sizeY = totalHeight / 100.0f;
-
-        for (int x = 0; x < 100; x++) {
-            for (int y = 0; y < 100; y++) {
-                float xPos = xOffset + (x * sizeX);
-                float yPos = yOffset + (y * sizeY);
-
-                GameObject go = new GameObject("Obj" + x + "" + y,
-                        new Transform(new Vector2f(xPos, yPos), new Vector2f(sizeX, sizeY)));
-                go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth, yPos / totalHeight, 1, 1)));
-                this.addGameObjectToScene(go);
-            }
-        }
-     */
 }
 
