2dc2e155f400abac89ef353747aafa82a0defe48
wnebyte <emilssonpost@gmail.com>
2022-08-01 19:35:20 +0700

    refactor

--- src/main/java/com/github/wnebyte/engine/components/KeyControls.java
+++ src/main/java/com/github/wnebyte/engine/components/KeyControls.java
@@ -5,44 +5,44 @@ import java.util.ArrayList;
 import org.joml.Vector2f;
 import com.github.wnebyte.engine.core.ecs.Component;
 import com.github.wnebyte.engine.core.ecs.GameObject;
-import com.github.wnebyte.engine.core.event.KeyListener;
 import com.github.wnebyte.engine.core.window.Window;
 import com.github.wnebyte.engine.util.Settings;
 import com.github.wnebyte.engine.editor.PropertiesWindow;
+import static com.github.wnebyte.engine.core.event.KeyListener.isKeyPressed;
+import static com.github.wnebyte.engine.core.event.KeyListener.keyBeginPress;
 import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyControls extends Component {
 
     @Override
     public void editorUpdate(float dt) {
-        PropertiesWindow propertiesWindow = Window.getImGuiLayer().getPropertiesWindow();
-        GameObject activeGameObject = propertiesWindow.getActiveGameObject();
-        List<GameObject> activeGameObjects = propertiesWindow.getActiveGameObjects();
+        PropertiesWindow props = Window.getImGuiLayer().getPropertiesWindow();
+        GameObject activeGameObject = props.getActiveGameObject();
+        List<GameObject> activeGameObjects = props.getActiveGameObjects();
 
-        // CTRL + D
-        if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) &&
-                KeyListener.keyBeginPress(GLFW_KEY_D) && activeGameObject != null) {
+        if (isKeyPressed(GLFW_KEY_LEFT_CONTROL) && keyBeginPress(GLFW_KEY_D) &&
+                activeGameObject != null) {
             GameObject newObject = activeGameObject.copy();
             Window.getScene().addGameObjectToScene(newObject);
             newObject.transform.position.add(new Vector2f(Settings.GRID_WIDTH, 0.0f));
-            propertiesWindow.setActiveGameObject(newObject);
+            props.setActiveGameObject(newObject);
             System.out.println("(Debug): Copy");
-        } // CTRL + D
-        else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL) &&
-                KeyListener.keyBeginPress(GLFW_KEY_D) && activeGameObjects.size() > 1) {
+        }
+        else if (isKeyPressed(GLFW_KEY_LEFT_CONTROL) && keyBeginPress(GLFW_KEY_D) &&
+                activeGameObjects.size() > 1) {
             List<GameObject> gameObjects = new ArrayList<>(activeGameObjects);
-            propertiesWindow.clearSelected();
+            props.clearSelected();
             for (GameObject go : gameObjects) {
                 GameObject copy = go.copy();
                 Window.getScene().addGameObjectToScene(copy);
-                propertiesWindow.addActiveGameObject(copy);
+                props.addActiveGameObject(copy);
             }
-        } // DELETE
-        else if (KeyListener.keyBeginPress(GLFW_KEY_DELETE)) {
+        }
+        else if (keyBeginPress(GLFW_KEY_DELETE)) {
             for (GameObject go : activeGameObjects) {
                 go.destroy();
             }
-            propertiesWindow.clearSelected();
+            props.clearSelected();
         }
     }
 }
